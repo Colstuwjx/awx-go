a8981e1e5c9063384ac9153ff6c28bf78504a1d4
Colstuwjx <Colstuwjx@gmail.com>
2018-06-23 11:05:20 +0800

    Fix typo.

--- job_template.go
+++ job_template.go
@@ -11,7 +11,7 @@ type JobTemplateService struct {
 	client *Client
 }
 
-func (this *PingService) Launch(id int, data map[string]interface{}, params map[string]string) (*Job, *http.Response, error) {
+func (this *JobTemplateService) Launch(id int, data map[string]interface{}, params map[string]string) (*Job, *http.Response, error) {
 	result := new(Job)
 	endpoint := fmt.Sprintf("/api/v2/job_templates/%d/launch/", id)
 	payload, err := json.Marshal(data)
