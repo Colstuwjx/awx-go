eb56e29aba97eea295cf8fdcb491b5ddbd90cae7
Colstuwjx <Colstuwjx@gmail.com>
2018-06-25 14:30:29 +0800

    Add test cases.

--- job_template_test.go
+++ job_template_test.go
@@ -189,14 +189,187 @@ func TestLauchJob(t *testing.T) {
 	var (
 		testJobTemplateId = 5
 		testInventoryId   = 1
+
+		expectLaunchJobTemplateResponse = &JobLauch{
+			Job:           499,
+			IgnoredFields: map[string]string{},
+			ID:            499,
+			Type:          "job",
+			URL:           "/api/v2/jobs/499/",
+			Related: &Related{
+				CreatedBy:          "/api/v2/users/1/",
+				ModifiedBy:         "/api/v2/users/1/",
+				Labels:             "/api/v2/jobs/499/labels/",
+				Inventory:          "/api/v2/inventories/1/",
+				Project:            "/api/v2/projects/4/",
+				Credential:         "/api/v2/credentials/1/",
+				ExtraCredentials:   "/api/v2/jobs/499/extra_credentials/",
+				Credentials:        "/api/v2/jobs/499/credentials/",
+				UnifiedJobTemplate: "/api/v2/job_templates/5/",
+				Stdout:             "/api/v2/jobs/499/stdout/",
+				Notifications:      "/api/v2/jobs/499/notifications/",
+				JobHostSummaries:   "/api/v2/jobs/499/job_host_summaries/",
+				JobEvents:          "/api/v2/jobs/499/job_events/",
+				ActivityStream:     "/api/v2/jobs/499/activity_stream/",
+				JobTemplate:        "/api/v2/job_templates/5/",
+				Cancel:             "/api/v2/jobs/499/cancel/",
+				CreateSchedule:     "/api/v2/jobs/499/create_schedule/",
+				Relaunch:           "/api/v2/jobs/499/relaunch/",
+			},
+
+			SummaryFields: &Summary{
+				JobTemplate: &JobTemplateSummary{
+					ID:          5,
+					Name:        "Demo Job Template",
+					Description: "",
+				},
+
+				Inventory: &Inventory{
+					ID:                           1,
+					Name:                         "Demo Inventory",
+					Description:                  "",
+					HasActiveFailures:            false,
+					TotalHosts:                   2,
+					HostsWithActiveFailures:      0,
+					TotalGroups:                  0,
+					GroupsWithActiveFailures:     0,
+					HasInventorySources:          false,
+					TotalInventorySources:        0,
+					InventorySourcesWithFailures: 0,
+					OrganizationID:               1,
+					Kind:                         "",
+				},
+
+				Credential: &Credential{
+					Description:      "",
+					CredentialTypeID: 1,
+					ID:               1,
+					Kind:             "ssh",
+					Name:             "Demo Credential",
+				},
+
+				UnifiedJobTemplate: &UnifiedJobTemplate{
+					ID:             5,
+					Name:           "Demo Job Template",
+					Description:    "",
+					UnifiedJobType: "job",
+				},
+
+				Project: &Project{
+					ID:          4,
+					Name:        "Demo Project",
+					Description: "",
+					Status:      "successful",
+					ScmType:     "git",
+				},
+
+				CreatedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				ModifiedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				UserCapabilities: &UserCapabilities{
+					Start:  true,
+					Delete: true,
+				},
+
+				Labels: &Labels{
+					Count:   0,
+					Results: []interface{}{},
+				},
+
+				ExtraCredentials: []interface{}{},
+				Credentials: []Credential{
+					Credential{
+						Description:      "",
+						CredentialTypeID: 1,
+						ID:               1,
+						Kind:             "ssh",
+						Name:             "Demo Credential",
+					},
+				},
+			},
+
+			Created: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-06-25T04:25:11.312072Z")
+				return t
+			}(),
+
+			Modified: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-06-25T04:25:11.362046Z")
+				return t
+			}(),
+
+			Name:                    "Demo Job Template",
+			Description:             "",
+			JobType:                 "run",
+			Inventory:               1,
+			Project:                 4,
+			Playbook:                "hello_world.yml",
+			Forks:                   0,
+			Limit:                   "",
+			Verbosity:               0,
+			ExtraVars:               "{}",
+			JobTags:                 "",
+			ForceHandlers:           false,
+			SkipTags:                "",
+			StartAtTask:             "",
+			Timeout:                 0,
+			UseFactCache:            false,
+			UnifiedJobTemplate:      5,
+			LaunchType:              "manual",
+			Status:                  "pending",
+			Failed:                  false,
+			Started:                 nil,
+			Finished:                nil,
+			Elapsed:                 0,
+			JobArgs:                 "",
+			JobCwd:                  "",
+			JobEnv:                  map[string]string{},
+			JobExplanation:          "",
+			ExecutionNode:           "",
+			ResultTraceback:         "",
+			EventProcessingFinished: false,
+			JobTemplate:             5,
+			PasswordsNeededToStart:  []interface{}{},
+			AskDiffModeOnLaunch:     false,
+			AskVariablesOnLaunch:    false,
+			AskLimitOnLaunch:        false,
+			AskTagsOnLaunch:         false,
+			AskSkipTagsOnLaunch:     false,
+			AskJobTypeOnLaunch:      false,
+			AskVerbosityOnLaunch:    false,
+			AskInventoryOnLaunch:    false,
+			AskCredentialOnLaunch:   false,
+			AllowSimultaneous:       false,
+			Artifacts:               map[string]string{},
+			ScmRevision:             "",
+			InstanceGroup:           nil,
+			DiffMode:                false,
+			Credential:              1,
+			VaultCredential:         nil,
+		}
 	)
 
 	awx := NewAWX(testAwxHost, testAwxUserName, testAwxPasswd, nil)
-	_, _, err := awx.JobTemplateService.Launch(testJobTemplateId, map[string]interface{}{
+	result, _, err := awx.JobTemplateService.Launch(testJobTemplateId, map[string]interface{}{
 		"inventory": testInventoryId,
 	}, map[string]string{})
 	if err != nil {
 		log.Fatalf("Lauch err: %s", err)
+	}
+
+	if !reflect.DeepEqual(*result, *expectLaunchJobTemplateResponse) {
+		log.Fatalf("LaunchJobTemplate resp not as expected, expected: %v, resp result: %v", *expectLaunchJobTemplateResponse, *result)
 	}
 
 	log.Println("Lauch passed!")
