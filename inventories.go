41f9612cba347d7f7ee73462c741067e19c6a195
Mauro Medda <mauromedda@users.noreply.github.com>
2018-08-19 14:21:11 +0200

    Add Inventories API endpoints (#6)
    * Add Inventories API endpoints
    
    * CreateInventory: create a new inventory
    * UpdateInventory: update an inventory
    * DeleteInventory: delete an inventory
    * GetInventory:    retrives inventory information by id
    
    The commit includes tests and usage examples.

--- inventories.go
+++ inventories.go
@@ -3,6 +3,7 @@ package awx
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 )
 
 // InventoriesService implements awx inventories apis.
@@ -41,7 +42,63 @@ func (i *InventoriesService) CreateInventory(data map[string]interface{}, params
 		return nil, err
 	}
 
+	// Add check if inventory exists and return proper error
+
 	resp, err := i.client.Requester.PostJSON(endpoint, bytes.NewReader(payload), result, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// UpdateInventory update an awx inventory
+func (i *InventoriesService) UpdateInventory(id int, data map[string]interface{}, params map[string]string) (*Inventory, error) {
+	result := new(Inventory)
+	endpoint := fmt.Sprintf("/api/v2/inventories/%d", id)
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := i.client.Requester.PatchJSON(endpoint, bytes.NewReader(payload), result, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// GetInventory retrives the inventory information from its ID or Name
+func (i *InventoriesService) GetInventory(id int, params map[string]string) (*Inventory, error) {
+	endpoint := fmt.Sprintf("/api/v2/inventories/%d", id)
+	result := new(Inventory)
+	resp, err := i.client.Requester.GetJSON(endpoint, result, map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := CheckResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+
+}
+
+// DeleteInventory delete an inventory from AWX
+func (i *InventoriesService) DeleteInventory(id int) (*Inventory, error) {
+	result := new(Inventory)
+	endpoint := fmt.Sprintf("/api/v2/inventories/%d", id)
+
+	resp, err := i.client.Requester.Delete(endpoint, result, nil)
 	if err != nil {
 		return nil, err
 	}
--- inventories_test.go
+++ inventories_test.go
@@ -276,3 +276,286 @@ func TestCreateInventory(t *testing.T) {
 
 	log.Println("CreateInventory passed!")
 }
+
+func TestUpdateInventory(t *testing.T) {
+	var (
+		expectUpdateInventoryResponse = &Inventory{
+			ID:   6,
+			Type: "inventory",
+			URL:  "/api/v2/inventories/6/",
+			Related: &Related{
+				NamedURL:       "/api/v2/inventories/TestInventory-update1++Default/",
+				CreatedBy:      "/api/v2/users/1/",
+				ModifiedBy:     "/api/v2/users/1/",
+				JobTemplates:   "/api/v2/inventories/6/job_templates/",
+				VariableData:   "/api/v2/inventories/6/variable_data/",
+				RootGroups:     "/api/v2/inventories/6/root_groups/",
+				ObjectRoles:    "/api/v2/inventories/6/object_roles/",
+				AdHocCommands:  "/api/v2/inventories/6/ad_hoc_commands/",
+				Script:         "/api/v2/inventories/6/script/",
+				Tree:           "/api/v2/inventories/6/tree/",
+				AccessList:     "/api/v2/inventories/6/access_list/",
+				ActivityStream: "/api/v2/inventories/6/activity_stream/",
+				InstanceGroups: "/api/v2/inventories/6/instance_groups/",
+				Hosts:          "/api/v2/inventories/6/hosts/",
+				Groups:         "/api/v2/inventories/6/groups/",
+				Copy:           "/api/v2/inventories/6/copy/",
+				UpdateInventorySources: "/api/v2/inventories/6/update_inventory_sources/",
+				InventorySources:       "/api/v2/inventories/6/inventory_sources/",
+				Organization:           "/api/v2/organizations/1/",
+			},
+			SummaryFields: &Summary{
+				Organization: &OrgnizationSummary{
+					ID:          1,
+					Name:        "Default",
+					Description: "",
+				},
+
+				CreatedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				ModifiedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				ObjectRoles: &ObjectRoles{
+					UseRole: &ApplyRole{
+						ID:          80,
+						Description: "Can use the inventory in a job template",
+						Name:        "Use",
+					},
+
+					AdminRole: &ApplyRole{
+						ID:          78,
+						Description: "Can manage all aspects of the inventory",
+						Name:        "Admin",
+					},
+
+					AdhocRole: &ApplyRole{
+						ID:          77,
+						Description: "May run ad hoc commands on an inventory",
+						Name:        "Ad Hoc",
+					},
+
+					UpdateRole: &ApplyRole{
+						ID:          81,
+						Description: "May update project or inventory or group using the configured source update system",
+						Name:        "Update",
+					},
+
+					ReadRole: &ApplyRole{
+						ID:          79,
+						Description: "May view settings for the inventory",
+						Name:        "Read",
+					},
+				},
+
+				UserCapabilities: &UserCapabilities{
+					Edit:   true,
+					Copy:   true,
+					Adhoc:  true,
+					Delete: true,
+				},
+			},
+
+			Created: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-08-13T01:59:47.160127Z")
+				return t
+			}(),
+
+			Modified: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-08-13T01:59:47.160140Z")
+				return t
+			}(),
+
+			Name:                         "TestInventory-update1",
+			Description:                  "for testing UpdateInventory api",
+			Organization:                 1,
+			Kind:                         "",
+			HostFilter:                   nil,
+			Variables:                    "",
+			HasActiveFailures:            false,
+			TotalHosts:                   0,
+			HostsWithActiveFailures:      0,
+			TotalGroups:                  0,
+			GroupsWithActiveFailures:     0,
+			HasInventorySources:          false,
+			TotalInventorySources:        0,
+			InventorySourcesWithFailures: 0,
+			InsightsCredential:           nil,
+			PendingDeletion:              false,
+		}
+	)
+
+	awx := NewAWX(testAwxHost, testAwxUserName, testAwxPasswd, nil)
+	result, err := awx.InventoriesService.UpdateInventory(6, map[string]interface{}{
+		"name":         "TestInventory-update1",
+		"description":  "for testing UpdateInventory api",
+		"organization": 1,
+		"kind":         "",
+		"host_filter":  "",
+		"variables":    "",
+	}, map[string]string{})
+	if err != nil {
+		log.Fatalf("UpdateInventory err: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expectUpdateInventoryResponse) {
+		log.Fatalf("UpdateInventory resp not as expected, expected: %v, resp result: %v", expectUpdateInventoryResponse, result)
+	}
+
+	log.Println("UpdateInventory passed!")
+}
+
+func TestGetInventory(t *testing.T) {
+	var (
+		expectGetInventoryResponse = &Inventory{
+			ID:   1,
+			Type: "inventory",
+			URL:  "/api/v2/inventories/1/",
+			Related: &Related{
+				CreatedBy:      "/api/v2/users/1/",
+				ModifiedBy:     "/api/v2/users/1/",
+				JobTemplates:   "/api/v2/inventories/1/job_templates/",
+				VariableData:   "/api/v2/inventories/1/variable_data/",
+				RootGroups:     "/api/v2/inventories/1/root_groups/",
+				ObjectRoles:    "/api/v2/inventories/1/object_roles/",
+				AdHocCommands:  "/api/v2/inventories/1/ad_hoc_commands/",
+				Script:         "/api/v2/inventories/1/script/",
+				Tree:           "/api/v2/inventories/1/tree/",
+				AccessList:     "/api/v2/inventories/1/access_list/",
+				ActivityStream: "/api/v2/inventories/1/activity_stream/",
+				InstanceGroups: "/api/v2/inventories/1/instance_groups/",
+				Hosts:          "/api/v2/inventories/1/hosts/",
+				Groups:         "/api/v2/inventories/1/groups/",
+				Copy:           "/api/v2/inventories/1/copy/",
+				UpdateInventorySources: "/api/v2/inventories/1/update_inventory_sources/",
+				InventorySources:       "/api/v2/inventories/1/inventory_sources/",
+				Organization:           "/api/v2/organizations/1/",
+			},
+			SummaryFields: &Summary{
+				Organization: &OrgnizationSummary{
+					ID:          1,
+					Name:        "Default",
+					Description: "",
+				},
+
+				CreatedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				ModifiedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+
+				ObjectRoles: &ObjectRoles{
+					UseRole: &ApplyRole{
+						ID:          80,
+						Description: "Can use the inventory in a job template",
+						Name:        "Use",
+					},
+
+					AdminRole: &ApplyRole{
+						ID:          78,
+						Description: "Can manage all aspects of the inventory",
+						Name:        "Admin",
+					},
+
+					AdhocRole: &ApplyRole{
+						ID:          77,
+						Description: "May run ad hoc commands on an inventory",
+						Name:        "Ad Hoc",
+					},
+
+					UpdateRole: &ApplyRole{
+						ID:          81,
+						Description: "May update project or inventory or group using the configured source update system",
+						Name:        "Update",
+					},
+
+					ReadRole: &ApplyRole{
+						ID:          79,
+						Description: "May view settings for the inventory",
+						Name:        "Read",
+					},
+				},
+
+				UserCapabilities: &UserCapabilities{
+					Edit:   true,
+					Copy:   true,
+					Adhoc:  true,
+					Delete: true,
+				},
+			},
+
+			Created: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-05-21T01:34:35.657185Z")
+				return t
+			}(),
+
+			Modified: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-05-30T09:42:22.412749Z")
+				return t
+			}(),
+
+			Name:                         "Demo Inventory",
+			Description:                  "",
+			Organization:                 1,
+			Kind:                         "",
+			HostFilter:                   nil,
+			Variables:                    "",
+			HasActiveFailures:            false,
+			TotalHosts:                   2,
+			HostsWithActiveFailures:      0,
+			TotalGroups:                  0,
+			GroupsWithActiveFailures:     0,
+			HasInventorySources:          false,
+			TotalInventorySources:        0,
+			InventorySourcesWithFailures: 0,
+			InsightsCredential:           nil,
+			PendingDeletion:              false,
+		}
+	)
+	awx := NewAWX(testAwxHost, testAwxUserName, testAwxPasswd, nil)
+	result, err := awx.InventoriesService.GetInventory(1, map[string]string{})
+	if err != nil {
+		log.Fatalf("GetInventory by ID err: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expectGetInventoryResponse) {
+		log.Fatalf("GetInventory by ID resp not as expected, expected: %v, resp result: %v", expectGetInventoryResponse, result)
+	}
+
+	log.Println("GetInventory by ID passed!")
+}
+
+func TestDeleteInventory(t *testing.T) {
+	var (
+		expectDeleteInventoryResponse = &Inventory{}
+	)
+
+	awx := NewAWX(testAwxHost, testAwxUserName, testAwxPasswd, nil)
+	result, err := awx.InventoriesService.DeleteInventory(1)
+	if err != nil {
+		log.Fatalf("DeleteInventory by ID err: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expectDeleteInventoryResponse) {
+		log.Fatalf("DeleteInventory resp not as expected, expected: %v, resp result: %v", expectDeleteInventoryResponse, result)
+	}
+
+	log.Println("DeleteInventory passed!")
+}
