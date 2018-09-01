9229e58a067ad32c2a4f4002084a7c721e5a733e
RoviLuca <Roviluca@users.noreply.github.com>
2018-09-01 14:07:51 +0200

    CreateJobTemplate with pretty output tests and added govendor. (#17)

--- job_template_test.go
+++ job_template_test.go
@@ -1,10 +1,17 @@
 package awx
 
 import (
-	"reflect"
 	"testing"
 	"time"
+
+	"github.com/kylelemons/godebug/pretty"
 )
+
+func checkAPICallResult(t *testing.T, expected interface{}, got interface{}) {
+	if diff := pretty.Compare(expected, got); diff != "" {
+		t.Fatalf("CreateJobTemplate diff: (-got +want)\n%s", diff)
+	}
+}
 
 func TestListJobTemplates(t *testing.T) {
 	var (
@@ -175,13 +182,9 @@ func TestListJobTemplates(t *testing.T) {
 	result, _, err := awx.JobTemplateService.ListJobTemplates(map[string]string{})
 
 	if err != nil {
-		t.Fatalf("ListJobTemplates err: %s", err)
-	} else if !reflect.DeepEqual(result, expectListJobTempaltesResponse) {
-		t.Logf("expected: %v", expectListJobTempaltesResponse)
-		t.Logf("result: %v", result)
-		t.Fatal("ListJobTemplates resp not as expected")
+		t.Logf("CreateJobTemplate err: %s", err)
 	} else {
-		t.Log("ListJobTemplates passed!")
+		checkAPICallResult(t, expectListJobTempaltesResponse, result)
 	}
 }
 
@@ -366,12 +369,171 @@ func TestLauchJob(t *testing.T) {
 	}, map[string]string{})
 
 	if err != nil {
-		t.Fatalf("Lauch err: %s", err)
-	} else if !reflect.DeepEqual(*result, *expectLaunchJobTemplateResponse) {
-		t.Logf("expected: %v", *expectLaunchJobTemplateResponse)
-		t.Logf("result: %v", *result)
-		t.Fatalf("LaunchJobTemplate resp not as expected")
+		t.Logf("CreateJobTemplate err: %s", err)
 	} else {
-		t.Log("Lauch passed!")
+		checkAPICallResult(t, expectLaunchJobTemplateResponse, result)
 	}
+}
+
+func TestCreateJobTemplate(t *testing.T) {
+	var (
+		expectCreateJobTempalteResponse = &JobTemplate{
+			ID:   5,
+			Type: "job_template",
+			URL:  "/api/v2/job_templates/5/",
+			Related: &Related{
+				NamedURL:                     "/api/v2/job_templates/Demo Job Template/",
+				CreatedBy:                    "/api/v2/users/1/",
+				ModifiedBy:                   "/api/v2/users/1/",
+				Labels:                       "/api/v2/job_templates/5/labels/",
+				Inventory:                    "/api/v2/inventories/1/",
+				Project:                      "/api/v2/projects/4/",
+				Credential:                   "/api/v2/credentials/1/",
+				ExtraCredentials:             "/api/v2/job_templates/5/extra_credentials/",
+				Credentials:                  "/api/v2/job_templates/5/credentials/",
+				NotificationTemplatesError:   "/api/v2/job_templates/5/notification_templates_error/",
+				NotificationTemplatesSuccess: "/api/v2/job_templates/5/notification_templates_success/",
+				NotificationTemplatesAny:     "/api/v2/job_templates/5/notification_templates_any/",
+				Jobs:           "/api/v2/job_templates/5/jobs/",
+				ObjectRoles:    "/api/v2/job_templates/5/object_roles/",
+				AccessList:     "/api/v2/job_templates/5/access_list/",
+				Launch:         "/api/v2/job_templates/5/launch/",
+				InstanceGroups: "/api/v2/job_templates/5/instance_groups/",
+				Schedules:      "/api/v2/job_templates/5/schedules/",
+				Copy:           "/api/v2/job_templates/5/copy/",
+				ActivityStream: "/api/v2/job_templates/5/activity_stream/",
+				SurveySpec:     "/api/v2/job_templates/5/survey_spec/",
+			},
+			SummaryFields: &Summary{
+				Inventory: &Inventory{
+					ID:                           1,
+					Name:                         "Demo Inventory",
+					Description:                  "",
+					HasActiveFailures:            false,
+					TotalHosts:                   2,
+					HostsWithActiveFailures:      0,
+					TotalGroups:                  0,
+					GroupsWithActiveFailures:     0,
+					HasInventorySources:          false,
+					TotalInventorySources:        0,
+					InventorySourcesWithFailures: 0,
+					OrganizationID:               1,
+					Kind:                         "",
+				},
+				Project: &Project{
+					ID:          4,
+					Name:        "Demo Project",
+					Description: "",
+					Status:      "never updated",
+					ScmType:     "git",
+				},
+				CreatedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+				ModifiedBy: &ByUserSummary{
+					ID:        1,
+					Username:  "admin",
+					FirstName: "",
+					LastName:  "",
+				},
+				ObjectRoles: &ObjectRoles{
+					AdminRole: &ApplyRole{
+						ID:          28,
+						Description: "Can manage all aspects of the job template",
+						Name:        "Admin",
+					},
+					ExecuteRole: &ApplyRole{
+						ID:          27,
+						Description: "May run the job template",
+						Name:        "Execute",
+					},
+					ReadRole: &ApplyRole{
+						ID:          26,
+						Description: "May view settings for the job template",
+						Name:        "Read",
+					},
+				},
+				UserCapabilities: &UserCapabilities{
+					Edit:     true,
+					Start:    true,
+					Copy:     true,
+					Schedule: true,
+					Delete:   true,
+				},
+				Labels: &Labels{
+					Count:   0,
+					Results: []interface{}{},
+				},
+				RecentJobs: []interface{}{},
+				Credentials: []Credential{
+					{
+						Description:      "",
+						CredentialTypeID: 1,
+						ID:               1,
+						Kind:             "ssh",
+						Name:             "Demo Credential",
+					},
+				},
+			},
+			Created: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-06-28T16:31:16.238510Z")
+				return t
+			}(),
+			Modified: func() time.Time {
+				t, _ := time.Parse(time.RFC3339, "2018-06-28T16:31:16.238524Z")
+				return t
+			}(),
+			Name:                  "Demo Job Template",
+			Description:           "",
+			JobType:               "run",
+			Inventory:             1,
+			Project:               4,
+			Playbook:              "hello_world.yml",
+			Forks:                 0,
+			Limit:                 "",
+			Verbosity:             0,
+			ExtraVars:             "",
+			JobTags:               "",
+			ForceHandlers:         false,
+			SkipTags:              "",
+			StartAtTask:           "",
+			Timeout:               0,
+			UseFactCache:          false,
+			LastJobRun:            nil,
+			LastJobFailed:         false,
+			NextJobRun:            nil,
+			Status:                "never updated",
+			HostConfigKey:         "",
+			AskDiffModeOnLaunch:   false,
+			AskVariablesOnLaunch:  false,
+			AskLimitOnLaunch:      false,
+			AskTagsOnLaunch:       false,
+			AskSkipTagsOnLaunch:   false,
+			AskJobTypeOnLaunch:    false,
+			AskVerbosityOnLaunch:  false,
+			AskInventoryOnLaunch:  false,
+			AskCredentialOnLaunch: false,
+			SurveyEnabled:         false,
+			BecomeEnabled:         false,
+			DiffMode:              false,
+			AllowSimultaneous:     false,
+			CustomVirtualenv:      nil,
+			Credential:            1,
+			VaultCredential:       nil,
+		}
+	)
+	awx := NewAWX(testAwxHost, testAwxUserName, testAwxPasswd, nil)
+	result, err := awx.JobTemplateService.CreateJobTemplate(map[string]interface{}{
+		"name":        "TestProject",
+		"description": "Test project",
+	}, map[string]string{})
+	if err != nil {
+		t.Logf("CreateJobTemplate err: %s", err)
+	} else {
+		checkAPICallResult(t, expectCreateJobTempalteResponse, result)
+	}
+	t.Log("CreateJobTemplate passed!")
 }
